1a83c4fae7d19c86c9b1d2315050420f8cf4fc46
Narizgnaw <26055055+Narizgnaw@users.noreply.github.com>
2023-12-23 20:54:52 +0800

    Update Needtobebanned IP at 2023-12-23 20:54:48

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ if ! command -v ipset &> /dev/null; then
     exit 1
 fi
 
-read -p "这个脚本将下载autoipset.sh并设置权限。请输入每隔多少小时执行一次脚本: " hours
+read -p "这个脚本将下载autoipset.sh并设置权限。请输入autoipset.sh的安装路径: " script_path
+
+read -p "请输入每隔多少小时执行一次脚本: " hours
 
 # 验证用户输入的是一个数字
 if [[ ! $hours =~ ^[0-9]+$ ]]; then
@@ -14,19 +16,16 @@ if [[ ! $hours =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-read -p "你确定要每隔 $hours 小时执行一次脚本吗？(y/n) " choice
+read -p "你确定要将autoipset.sh安装到 $script_path，并每隔 $hours 小时执行一次脚本吗？(y/n) " choice
 if [[ $choice == "y" || $choice == "Y" ]]; then
     # 下载autoipset.sh脚本
-    wget https://narizgnaw.github.io/needtobebanned/autoipset.sh
+    wget https://narizgnaw.github.io/needtobebanned/autoipset.sh -O "$script_path/autoipset.sh"
 
     # 设置脚本权限
-    chmod +x autoipset.sh
-
-    # 手动输入脚本的安装路径
-    read -p "请输入autoipset.sh的安装路径: " script_path
+    chmod +x "$script_path/autoipset.sh"
 
     # 添加crontab任务
-    (crontab -l ; echo "0 */$hours * * * $script_path") | crontab -
+    (crontab -l ; echo "0 */$hours * * * sh $script_path/autoipset.sh") | crontab -
 else
     echo "安装已取消"
 fi
